811b2795a56849fbed019d39248aa9b351445b5a
Jade Simien <baba3of4@gmail.com>
2023-06-03 19:21:10 -0700

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Jade Simien on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
